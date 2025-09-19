7dcab128378b26ec773aece7e978b07447d31e3d
emmi0122 <emiliaidajarleback@gmail.com>
2025-09-19 14:01:51 +0200

    Updating Main class

--- src/main/java/se/berg/Main.java
+++ src/main/java/se/berg/Main.java
@@ -2,13 +2,23 @@ package se.berg;
 
 import se.berg.domain.TestSequence;
 import se.berg.io.JsonParser;
+import se.berg.service.HtmlReportGenerator;
 import se.berg.service.TestSequencePrinter;
 
 public class Main {
     public static void main(String[] args) {
         try {
+            //Parse JSON-file
             TestSequence seq = JsonParser.parseTestSequence("emilia_test_seq.json");
+
+            //Print everything to textfile
             TestSequencePrinter.print(seq, "parsed-json.txt");
+
+            //Generate HTML report
+            HtmlReportGenerator.generateReport(seq, "test-report.html");
+            System.out.println("Reports generated succesfully!");
+            System.out.println("- Text report: parsed-json.txt");
+            System.out.println("- HTML report: test-report.html");
         } catch (Exception e) {
             e.printStackTrace();
         }
